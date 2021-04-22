100d77f548da468c710a10d755a9d5ee13a07f1a
Ziska <franziska@baerthele.de>
2021-04-22 11:53:01 +0200

    reformat code but not yet finished

--- Classifier.py
+++ Classifier.py
@@ -57,84 +57,77 @@ dataset = dataset[(dataset.days_b_screening_arrest <= 30) &
 dataset.reset_index(inplace=True, drop=True) # renumber the rows from 0 again
 
 
-# outputting
+# turn into a binary classification problem
+# create feature is_med_or_high_risk
+dataset['age_cat_binary'] = (dataset['age']<=35).astype(int)           # below 35 y.o = 1 / above 35 = 0
+dataset['sex_binary'] = (dataset['sex']=='Male').astype(int)           # male = 1, female = 0
+dataset['charge_degree_binary'] = (dataset['c_charge_degree']=='F').astype(int)   #felony = 1 / misdemeanor = 0
+dataset['is_5_or_more_decile_score']  = (dataset['decile_score']>5).astype(int)
 
-def printresult(testarray, modelarray):
-    c=0
-    for i,j in zip(testarray, modelarray):
-        if c<20:
-            print(i, ' - ', j)
-        else:
-            break
-        c += 1
+DSX_train, DSX_test, DSy_train, DSy_test = train_test_split(dataset, dataset, test_size=0.3, shuffle=True, random_state=33)
 
 
-
-X = dataset[['age', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count', 'is_recid', 'is_violent_recid']]
+#X = dataset[['age', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count', 'is_recid', 'is_violent_recid']]
+X = dataset[['age_cat_binary', 'is_recid', 'juv_fel_count', 'juv_misd_count', 'priors_count', 'charge_degree_binary']]
+#X = dataset[['is_5_or_more_decile_score', 'is_med_or_high_risk', 'age_cat_binary', 'sex_binary']]
+#X = dataset[['age', 'juv_fel_count', 'juv_misd_count', 'priors_count', 'is_recid','charge_degree_binary', 'sex_binary']]
 y = dataset.two_year_recid
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, stratify=y, random_state=33)
 
-#print(y_test.keys)
 
+# cnt_caucasian = sum(dataset.race == 'Caucasian')
+# print(cnt_caucasian)
+# cnt_african_american = sum(dataset.race == 'African-American')
+# print(cnt_african_american)
+# cnt_asian = sum(dataset.race == 'Asian')
+# cnt_other = sum(dataset.race == 'Other')
+# cnt_hispanic = sum(dataset.race == 'Hispanic')
+# cnt_native_american = sum(dataset.race == 'Native-American')
 
-# Gausian Naive Bayes Classifier -> 97%
-print('GausianNB')
+# preparing dfs
+df_african_american = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'is_5_or_more_decile_score', 'gnb', 'kNN', 'dtc'])
+df_caucasian = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'is_5_or_more_decile_score', 'gnb', 'kNN', 'dtc'])
+df_male = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'is_5_or_more_decile_score', 'gnb', 'kNN', 'dtc'])
+df_female = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'is_5_or_more_decile_score', 'gnb', 'kNN', 'dtc'])
+
+df_concl = pd.DataFrame(DSX_test, columns=['id', 'sex', 'race', 'two_year_recid', 'decile_score', 'is_5_or_more_decile_score'])
+# df_concl['gnb'] = total_gnb
+# df_concl['kNN'] = total_knn
+# df_concl['dtc'] = total_dtc
+
+# Classifiers
 gnb = GaussianNB()
-gnb.fit(X_train, y_train)
-y_gnb = gnb.predict(X_test)
-score_gnb = accuracy_score(y_test, y_gnb)
-print('accuracy score gnb: ', score_gnb)
-#print(confusion_matrix(y_test, y_gnb))
-#printresult(y_test, y_gnb)
-total_gnb = gnb.predict(X)
-print(confusion_matrix(y, total_gnb))
-#printresult(y, total_gnb)
-
-
-#kNN - K=3 -> 93% slightly better than 2 (91%) what doesn't make sense.
-print('kNN')
 K = 3
 knn = KNeighborsClassifier(n_neighbors = K)
-knn.fit(X_train, y_train)
-y_knn = knn.predict(X_test)
-print('accuracy score knn: ', accuracy_score(y_test, y_knn))
-#print(confusion_matrix(y_test, y_knn))
-#printresult(y_test, y_knn)
-total_knn = knn.predict(X)
-print(confusion_matrix(y, total_knn))
-#printresult(y, total_knn)
-
-# DecisionTree
-print('\nDecisionTree')
 dtc = DecisionTreeClassifier()
-dtc.fit(X_train, y_train)
-y_dtc = dtc.predict(X_test)
-print('accuracy score dtc: ', accuracy_score(y_test, y_dtc))
-total_dtc = dtc.predict(X)
-print(confusion_matrix(y, total_dtc))
+classifiers = [gnb, knn, dtc]
 
+# methods
+def fitting(classifier):
+    classifier.fit(X_train, y_train)
 
+def predicting(classifier):
+    y_model = classifier.predict(X_test)
+    print('accuracy score ', type(classifier), ': ', accuracy_score(y_test, y_model))
+    conf = confusion_matrix(y_test, y_model)
+    print(conf)
+    print('fp: ', conf[0, 1], '\nfn: ', conf[1, 0])
+    print('fp: ', (conf[0, 1]/(conf[0,1]+conf[0,0]))*100, '\nfn: ', (conf[1, 0]/(conf[1,1]+conf[0,0]))*100)
+    if type(classifier) == GaussianNB:
+        df_concl['gnb'] = y_model
+    elif type(classifier) == KNeighborsClassifier:
+        df_concl['kNN'] = y_model
+    elif type(classifier) == DecisionTreeClassifier:
+        df_concl['dtc'] = y_model
 
+for i in classifiers:
+    fitting(i)
+    predicting(i)
 
 ################### look at fairness
 
-df_african_american = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'compas', 'gnb', 'kNN', 'dtc'])
-df_caucasian = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'compas', 'gnb', 'kNN', 'dtc'])
-df_male = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'compas', 'gnb', 'kNN', 'dtc'])
-df_female = pd.DataFrame(None, columns=['id', 'race', 'two_year_recid', 'decile_score', 'compas', 'gnb', 'kNN', 'dtc'])
-
-df_concl = pd.DataFrame(dataset, columns=['id', 'sex', 'race', 'two_year_recid', 'decile_score', 'compas'])
-df_concl['gnb'] = total_gnb
-df_concl['kNN'] = total_knn
-df_concl['dtc'] = total_dtc
-
 for index, row in df_concl.iterrows():
-    if row['decile_score'] > 5:
-        df_concl.loc[index, 'compas'] = int(1)
-    elif row['decile_score'] <= 5:
-        df_concl.loc[index, 'compas'] = int(0)
     if row['race'] == 'Caucasian':
         df_caucasian = df_caucasian.append(row, ignore_index=True)
     elif row['race'] == 'African-American':
@@ -144,42 +137,47 @@ for index, row in df_concl.iterrows():
     elif row['sex'] == 'Female':
         df_female = df_female.append(row, ignore_index=True)
 
-df_concl = df_concl.astype({'compas': np.int64})
+print(df_concl)
+print(df_caucasian)
+print(df_african_american)
+print(df_male)
+print(df_female)
 
-print('\n compas accuracy: ', accuracy_score(df_concl.two_year_recid, df_concl.compas))
 
-for index, row in df_african_american.iterrows():
-    if row['decile_score'] > 5:
-        row['compas'] = int(1)
-    elif row['decile_score'] <= 5:
-        row['compas'] = int(0)
+print('\n compas accuracy: ', accuracy_score(df_concl.two_year_recid, df_concl.is_5_or_more_decile_score))
 
-for index, row in df_caucasian.iterrows():
-    if row['decile_score'] > 5:
-        row['compas'] = int(1)
-    elif row['decile_score'] <= 5:
-        row['compas'] = int(0)
-
-for index, row in df_male.iterrows():
-    if row['decile_score'] > 5:
-        row['compas'] = int(1)
-    elif row['decile_score'] <= 5:
-        row['compas'] = int(0)
-
-for index, row in df_female.iterrows():
-    if row['decile_score'] > 5:
-        row['compas'] = int(1)
-    elif row['decile_score'] <= 5:
-        row['compas'] = int(0)
+# for index, row in df_african_american.iterrows():
+#     if row['decile_score'] > 5:
+#         row['compas'] = int(1)
+#     elif row['decile_score'] <= 5:
+#         row['compas'] = int(0)
+#
+# for index, row in df_caucasian.iterrows():
+#     if row['decile_score'] > 5:
+#         row['compas'] = int(1)
+#     elif row['decile_score'] <= 5:
+#         row['compas'] = int(0)
+#
+# for index, row in df_male.iterrows():
+#     if row['decile_score'] > 5:
+#         row['compas'] = int(1)
+#     elif row['decile_score'] <= 5:
+#         row['compas'] = int(0)
+#
+# for index, row in df_female.iterrows():
+#     if row['decile_score'] > 5:
+#         row['compas'] = int(1)
+#     elif row['decile_score'] <= 5:
+#         row['compas'] = int(0)
 
 #compas
 print('\n', 'compas: white then black, men then women')
-confusion_matrix = pd.crosstab(df_caucasian['two_year_recid'], df_caucasian['compas'], rownames=['Actual'], colnames=['Predicted'])
+confusion_matrix = pd.crosstab(df_caucasian['two_year_recid'], df_caucasian['is_5_or_more_decile_score'], rownames=['Actual'], colnames=['Predicted'])
 print('\n', confusion_matrix)
-confusion_matrix = pd.crosstab(df_african_american['two_year_recid'], df_african_american['compas'], rownames=['Actual'], colnames=['Predicted'])
+confusion_matrix = pd.crosstab(df_african_american['two_year_recid'], df_african_american['is_5_or_more_decile_score'], rownames=['Actual'], colnames=['Predicted'])
 print('\n', confusion_matrix)
-print('\n', pd.crosstab(df_male['two_year_recid'], df_male['compas'], rownames=['Actual'], colnames=['Predicted']))
-print('\n', pd.crosstab(df_female['two_year_recid'], df_female['compas'], rownames=['Actual'], colnames=['Predicted']))
+print('\n', pd.crosstab(df_male['two_year_recid'], df_male['is_5_or_more_decile_score'], rownames=['Actual'], colnames=['Predicted']))
+print('\n', pd.crosstab(df_female['two_year_recid'], df_female['is_5_or_more_decile_score'], rownames=['Actual'], colnames=['Predicted']))
 
 #gnb
 print('\n', 'gnb: white then black, men then women')
